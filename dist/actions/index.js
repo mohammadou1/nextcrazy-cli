1fe2a3bda5be27b8a4690afb2fc8587ed08cda10
mohux <57658626+mohux@users.noreply.github.com>
2020-09-20 19:36:05 +0300

    deleted dist

--- dist/actions/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-tslib_1.__exportStar(require("./page.action"), exports);
-tslib_1.__exportStar(require("./component.action"), exports);
-tslib_1.__exportStar(require("./language.action"), exports);
